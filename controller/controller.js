507b0c60c5338ee772a1b0add18e7615b0961d3d
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-12-24 20:13:11 +0530

    Update controller.js

--- controller/controller.js
+++ controller/controller.js
@@ -1,4 +1,4 @@
-const {dataone,datafashion, sportToyBaby, datapremium, sportToyBags, groceriesBeveragesCoffee, groceriesBeveragesTea, women, menData,sportdata, Home, pears, premiumApple }=require ("../data/data1")
+const {dataone,datafashion, datalaptop, women, menData,accesoriesdata, Home, laptopApple }=require ("../data/data1")
 
 const blogController=((req,res)=>{
     res.send(dataone)
@@ -9,31 +9,31 @@ const fashiondata=((req,res)=>{
 
 })
 
-const sportbaby=((req,res)=>{
-    res.send(sportToyBaby)
+// const sportbaby=((req,res)=>{
+//     res.send(sportToyBaby)
+
+// })
+
+const laptop=((req,res)=>{
+    res.send(datalaptop)
 
 })
 
-const premium=((req,res)=>{
-    res.send(datapremium)
+// const sportBags=((req,res)=>{
+//     res.send(sportToyBags)
 
-})
+// })
 
-const sportBags=((req,res)=>{
-    res.send(sportToyBags)
+// const BeverageCoffee=((req,res)=>{
+//     res.send(groceriesBeveragesCoffee)
 
-})
-
-const BeverageCoffee=((req,res)=>{
-    res.send(groceriesBeveragesCoffee)
-
-})
+// })
 
 
-const BeverageTea=((req,res)=>{
-    res.send(groceriesBeveragesTea)
+// const BeverageTea=((req,res)=>{
+//     res.send(groceriesBeveragesTea)
 
-})
+// })
 
 
 const womenData=((req,res)=>{
@@ -49,8 +49,8 @@ const men=((req,res)=>{
 })
 
 
-const datadsport=((req,res)=>{
-    res.send(sportdata)
+const dataaccesories=((req,res)=>{
+    res.send(accesoriesdata)
 
 })
 
@@ -59,12 +59,12 @@ const HomeData=((req,res)=>{
     res.send(Home)
 })
 
-const PearData=((req,res)=>{
-    res.send(pears)
-})
+// const PearData=((req,res)=>{
+//     res.send(pears)
+// })
 
 const AppleData=((req,res)=>{
-    res.send(premiumApple)
+    res.send(laptopApple)
 })
 
 
@@ -73,4 +73,4 @@ const AppleData=((req,res)=>{
 
 
 
-module.exports={blogController,sportBags,premium,sportbaby,fashiondata,BeverageCoffee,BeverageTea,womenData,men,datadsport,HomeData,PearData,AppleData}
+module.exports={blogController,laptop,fashiondata,womenData,men,dataaccesories,HomeData,AppleData}
